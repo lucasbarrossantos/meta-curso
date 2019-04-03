fa938787eac8b17f174f93ad007202729803e040
Lucas Barros <lucas-barros28@hotmail.com>
2019-04-03 20:16:59 -0300

    - TurmasResource Ok

---
+++ src/main/java/com/metacurso/repository/CursosTurmasRepository.java
@@ -0,0 +1,8 @@
+package com.metacurso.repository;
+
+import com.metacurso.model.CursosTurmas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursosTurmasRepository extends JpaRepository<CursosTurmas, Integer> {
+
+}
--- src/main/java/com/metacurso/service/TurmaService.java
+++ src/main/java/com/metacurso/service/TurmaService.java
@@ -1,13 +1,16 @@
 package com.metacurso.service;
 
+import com.metacurso.model.CursosTurmas;
 import com.metacurso.model.Turmas;
 import com.metacurso.repository.CursoRepository;
+import com.metacurso.repository.CursosTurmasRepository;
 import com.metacurso.repository.TurmaRepository;
 import com.metacurso.service.exception.CursoInexistenteException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -20,11 +23,16 @@ public class TurmaService {
     @Autowired
     private CursoRepository cursoRepository;
 
+    @Autowired
+    private CursosTurmasRepository cursosTurmasRepository;
+
     public Turmas save(Turmas turmas) {
         validarCurso(turmas);
 
         turmas.setVagasdisponiveis(turmas.getNvagas());
-        return turmaRepository.save(turmas);
+        turmas = turmaRepository.save(turmas);
+        cursoTurma(turmas);
+        return turmas;
     }
 
     public Turmas update(Integer codigo, Turmas turmas) {
@@ -47,5 +55,13 @@ public class TurmaService {
     private void validarCurso(Turmas turmas) {
         cursoRepository.findById(turmas.getCurso().getCodigo())
                 .orElseThrow(CursoInexistenteException::new);
+    }
+
+    @Transactional
+    public void cursoTurma(Turmas turmas) {
+        CursosTurmas cursosTurmas = new CursosTurmas();
+        cursosTurmas.setTurma(turmas);
+        cursosTurmas.setCurso(turmas.getCurso());
+        cursosTurmasRepository.save(cursosTurmas);
     }
 }
